8f254bd8a4900ca91cd9a056a64e6a2657abcb63
Levi Menezes <leviditomazzo@gmail.com>
2020-07-01 13:25:00 -0300

    using configmap for insert container

--- mutate.js
+++ mutate.js
@@ -1,28 +1,10 @@
+const fs = require('fs');
+
 const mutate = (req, res, next) => {
 
-    const authz_container = {
-        "image": "leviditomazzo/authz:v1",
-        "name": "authz",
-        "ports": [{
-            "containerPort": 50051
-        }]
-    }
+    const inject = fs.readFileSync('/configMap/inject.json').toString();
 
-    const label = {
-        "op": "add",
-        "path": "/metadata/labels/securityTypeInBound",
-        "value": "enable"
-    }
-
-    const container = {
-        "op": "add",
-        "path": "/spec/containers/-",
-        "value": authz_container,
-    }
-
-    const patch = [label, container]
-
-    const patchBase64 = Buffer.from(JSON.stringify(patch)).toString("base64");
+    const patchBase64 = Buffer.from(inject).toString("base64");
 
     const result = {
         response: {
